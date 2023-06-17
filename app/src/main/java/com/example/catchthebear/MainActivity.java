e6bfb6520fc56e717782b39c1319c30d06ee163b
ahmetalpsamur <ahmetalpsamur@gmail.com>
2023-06-17 14:11:01 +0300

    MainActivity draft

--- app/src/main/java/com/example/catchthebear/MainActivity.java
+++ app/src/main/java/com/example/catchthebear/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.catchthebear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewSaved;
+    Button buttonExit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
